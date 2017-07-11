3fccd0c602577bda904e83884579da7a836ba4db
Fredrik Arvidsson <fresa@fresa.se>
2017-07-11 22:42:10 +0200

    lazy loading logger creation for less chance of cyclic dependecy issues when using advanced custom targets etc

--- Logging.NLog/NLogLogger.cs
+++ Logging.NLog/NLogLogger.cs
@@ -5,202 +5,201 @@ namespace Log.It.With.NLog
 {
     public class NLogLogger : ILogger
     {
-        private readonly Logger _logger;
-        private readonly ILogContext _logContext;
+        private readonly Lazy<Logger> _lazyLoggerResolver;
+        private Logger Logger => _lazyLoggerResolver.Value;
 
         public NLogLogger(string type, ILogContext logContext)
         {
-            _logger = LogManager.GetLogger(type);
-            _logContext = logContext;
+            _lazyLoggerResolver = new Lazy<Logger>(() => LogManager.GetLogger(type));
+            LogicalThread = logContext;
         }
-
 
         public void Fatal(Exception ex, IFormatProvider formatProvider, string template, params object[] args)
         {
-            _logger.Fatal(ex, formatProvider, template, args);
+            Logger.Fatal(ex, formatProvider, template, args);
         }
 
         public void Fatal(Exception ex, string template, params object[] args)
         {
-            _logger.Fatal(ex, template, args);
+            Logger.Fatal(ex, template, args);
         }
 
         public void Fatal<T>(IFormatProvider formatProvider, T msg)
         {
-            _logger.Fatal(formatProvider, msg);
+            Logger.Fatal(formatProvider, msg);
         }
 
         public void Fatal(IFormatProvider formatProvider, object msg)
         {
-            _logger.Fatal(formatProvider, msg);
+            Logger.Fatal(formatProvider, msg);
         }
 
         public void Fatal(Exception ex, string msg)
         {
-            _logger.Fatal(ex, msg);
+            Logger.Fatal(ex, msg);
         }
 
         public void Fatal(string template, params object[] args)
         {
-            _logger.Fatal(template, args);
+            Logger.Fatal(template, args);
         }
 
-        
+
         public void Error(Exception ex, IFormatProvider formatProvider, string template, params object[] args)
         {
-            _logger.Error(ex, formatProvider, template, args);
+            Logger.Error(ex, formatProvider, template, args);
         }
-        
+
         public void Error(Exception ex, string template, params object[] args)
         {
-            _logger.Error(ex, template, args);
+            Logger.Error(ex, template, args);
         }
 
         public void Error<TMessage>(IFormatProvider formatProvider, TMessage msg)
         {
-            _logger.Error(formatProvider, msg);
+            Logger.Error(formatProvider, msg);
         }
 
         public void Error(IFormatProvider formatProvider, object msg)
         {
-            _logger.Error(formatProvider, msg);
+            Logger.Error(formatProvider, msg);
         }
 
         public void Error(Exception ex, string msg)
         {
-            _logger.Fatal(ex, msg);
+            Logger.Fatal(ex, msg);
         }
 
         public void Error(string template, params object[] args)
         {
-            _logger.Error(template, args);
+            Logger.Error(template, args);
         }
 
 
         public void Warning(Exception ex, IFormatProvider formatProvider, string template, params object[] args)
         {
-            _logger.Warn(ex, formatProvider, template, args);
+            Logger.Warn(ex, formatProvider, template, args);
         }
-        
+
         public void Warning(Exception ex, string template, params object[] args)
         {
-            _logger.Warn(ex, template, args);
+            Logger.Warn(ex, template, args);
         }
 
         public void Warning<TMessage>(IFormatProvider formatProvider, TMessage msg)
         {
-            _logger.Warn(formatProvider, msg);
+            Logger.Warn(formatProvider, msg);
         }
 
         public void Warning(IFormatProvider formatProvider, object msg)
         {
-            _logger.Warn(formatProvider, msg);
+            Logger.Warn(formatProvider, msg);
         }
 
         public void Warning(Exception ex, string msg)
         {
-            _logger.Warn(ex, msg);
+            Logger.Warn(ex, msg);
         }
 
         public void Warning(string template, params object[] args)
         {
-            _logger.Warn(template, args);
+            Logger.Warn(template, args);
         }
 
 
         public void Info(Exception ex, IFormatProvider formatProvider, string template, params object[] args)
         {
-            _logger.Info(ex, formatProvider, template, args);
+            Logger.Info(ex, formatProvider, template, args);
         }
 
         public void Info(Exception ex, string template, params object[] args)
         {
-            _logger.Info(ex, template, args);
+            Logger.Info(ex, template, args);
         }
 
         public void Info<TMessage>(IFormatProvider formatProvider, TMessage msg)
         {
-            _logger.Info(formatProvider, msg);
+            Logger.Info(formatProvider, msg);
         }
 
         public void Info(IFormatProvider formatProvider, object msg)
         {
-            _logger.Info(formatProvider, msg);
+            Logger.Info(formatProvider, msg);
         }
 
         public void Info(Exception ex, string msg)
         {
-            _logger.Info(ex, msg);
+            Logger.Info(ex, msg);
         }
 
         public void Info(string template, params object[] args)
         {
-            _logger.Info(template, args);
+            Logger.Info(template, args);
         }
 
 
         public void Debug(Exception ex, IFormatProvider formatProvider, string template, params object[] args)
         {
-            _logger.Debug(ex, formatProvider, template, args);
+            Logger.Debug(ex, formatProvider, template, args);
         }
 
         public void Debug(Exception ex, string template, params object[] args)
         {
-            _logger.Debug(ex, template, args);
+            Logger.Debug(ex, template, args);
         }
 
         public void Debug<TMessage>(IFormatProvider formatProvider, TMessage msg)
         {
-            _logger.Debug(formatProvider, msg);
+            Logger.Debug(formatProvider, msg);
         }
 
         public void Debug(IFormatProvider formatProvider, object msg)
         {
-            _logger.Debug(formatProvider, msg);
+            Logger.Debug(formatProvider, msg);
         }
 
         public void Debug(Exception ex, string msg)
         {
-            _logger.Debug(ex, msg);
+            Logger.Debug(ex, msg);
         }
-        
+
         public void Debug(string template, params object[] args)
         {
-            _logger.Debug(template, args);
+            Logger.Debug(template, args);
         }
 
 
         public void Trace(Exception ex, IFormatProvider formatProvider, string template, params object[] args)
         {
-            _logger.Trace(ex, formatProvider, template, args);
+            Logger.Trace(ex, formatProvider, template, args);
         }
 
         public void Trace(Exception ex, string template, params object[] args)
         {
-            _logger.Trace(ex, template, args);
+            Logger.Trace(ex, template, args);
         }
 
         public void Trace<TMessage>(IFormatProvider formatProvider, TMessage msg)
         {
-            _logger.Trace(formatProvider, msg);
+            Logger.Trace(formatProvider, msg);
         }
 
         public void Trace(IFormatProvider formatProvider, object msg)
         {
-            _logger.Trace(formatProvider, msg);
+            Logger.Trace(formatProvider, msg);
         }
 
         public void Trace(Exception ex, string msg)
         {
-            _logger.Trace(ex, msg);
+            Logger.Trace(ex, msg);
         }
-        
+
         public void Trace(string template, params object[] args)
         {
-            _logger.Trace(template, args);
+            Logger.Trace(template, args);
         }
 
 
-        public ILogContext LogicalThread => _logContext;
+        public ILogContext LogicalThread { get; }
     }
 }
--- Tests/Logging.NLog.Tests/When_rendering_using_the_log_context_layout.cs
+++ Tests/Logging.NLog.Tests/When_rendering_using_the_log_context_layout.cs
@@ -1,20 +1,34 @@
-﻿using FakeItEasy;
+﻿using System;
+using FakeItEasy;
+using NLog;
 using NLog.Config;
+using Should.Fluent;
+using Test.It.With.XUnit;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Log.It.With.NLog.Tests
 {
-    public class When_rendering_using_the_log_context_layout : XUnitSpecification
+    public class When_rendering_using_the_log_context_layout : XUnit2Specification
     {
         private NLogLogger _logger;
         private IWrite _writer;
+        private bool _created;
+        private bool _configurationObjectsCreatedAfterInstantiating;
+        private ConfigurationItemCreator _defaultInstanceCreator;
+
+        public When_rendering_using_the_log_context_layout(ITestOutputHelper output) : base(output)
+        {
+        }
 
         protected override void Given()
         {
             _writer = A.Fake<IWrite>();
-            var defaultInstanceCreator = ConfigurationItemFactory.Default.CreateInstance;
+
+            _defaultInstanceCreator = ConfigurationItemFactory.Default.CreateInstance;
             ConfigurationItemFactory.Default.CreateInstance = type =>
             {
+                _created = true;
                 if (type == typeof(TestTarget))
                 {
                     return new TestTarget(_writer);
@@ -23,10 +37,25 @@ namespace Log.It.With.NLog.Tests
                 {
                     return new NLogLogContextLayoutRenderer(new LogicalThreadContext());
                 }
-                return defaultInstanceCreator(type);
+                if (type == typeof(XUnit2Target))
+                {
+                    return new XUnit2Target(Output);
+                }
+                return _defaultInstanceCreator(type);
             };
+
             _logger = new NLogLogger("TestType", new LogicalThreadContext());
+            _configurationObjectsCreatedAfterInstantiating = _created;
             _logger.LogicalThread.Set("item1", "value1");
+
+            OnDisposing += (sender, args) => Reset();
+            LogManager.Configuration = LogManager.Configuration.Reload();
+        }
+
+        private void Reset()
+        {
+            ConfigurationItemFactory.Default.CreateInstance = _defaultInstanceCreator;
+            LogManager.Shutdown();
         }
 
         protected override void When()
@@ -38,6 +67,18 @@ namespace Log.It.With.NLog.Tests
         public void It_should_have_writen_item1_but_not_item2()
         {
             A.CallTo(() => _writer.Write("item1=value1, item2=")).MustHaveHappened();
+        }
+
+        [Fact]
+        public void It_should_not_have_created_nlog_configuration_objects_when_the_logger_has_not_logged_anything()
+        {
+            _configurationObjectsCreatedAfterInstantiating.Should().Be.False();
+        }
+
+        [Fact]
+        public void It_should_have_created_nlog_configuration_objects()
+        {
+            _created.Should().Be.True();
         }
     }
 }
