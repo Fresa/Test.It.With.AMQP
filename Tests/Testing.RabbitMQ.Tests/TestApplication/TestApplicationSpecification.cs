1b875041e3e096a4bddfd9276cf5b5163d0ca961
Fresa <fresa@fresa.se>
2017-12-12 00:06:40 +0100

    let the test application close connection when finished publishing a message

--- Tests/Testing.RabbitMQ.Tests/TestApplication/TestApplicationSpecification.cs
+++ Tests/Testing.RabbitMQ.Tests/TestApplication/TestApplicationSpecification.cs
@@ -8,7 +8,6 @@ namespace Test.It.With.RabbitMQ.Tests.TestApplication
     public class TestApplicationSpecification
     {
         private SimpleInjectorDependencyResolver _configurer;
-        private IMessagePublisher _messagePublisher;
 
         public void Configure(Action<SimpleInjectorDependencyResolver> reconfigurer)
         {
@@ -25,14 +24,15 @@ namespace Test.It.With.RabbitMQ.Tests.TestApplication
         public void Start()
         {
             var messagePublisherFactory = _configurer.Resolve<IMessagePublisherFactory>();
-            _messagePublisher = messagePublisherFactory.Create("myExchange");
 
-            _messagePublisher.Publish("myMessage", new TestMessage("Testing sending a message using RabbitMQ"));
+            using (var messagePublisher = messagePublisherFactory.Create("myExchange"))
+            {
+                messagePublisher.Publish("myMessage", new TestMessage("Testing sending a message using RabbitMQ"));
+            }
         }
 
         public void Stop()
         {
-            _messagePublisher?.Dispose();
             _configurer.Dispose();
         }
     }
