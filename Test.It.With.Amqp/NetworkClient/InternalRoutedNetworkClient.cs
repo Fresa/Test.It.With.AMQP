3ed8c5cdd50817988c50546e4454d4357254588b
Fresa <fresa@fresa.se>
2018-01-29 17:54:48 +0100

    removed unnecessary logging

--- Test.It.With.Amqp/NetworkClient/InternalRoutedNetworkClient.cs
+++ Test.It.With.Amqp/NetworkClient/InternalRoutedNetworkClient.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Text;
-using Log.It;
 
 namespace Test.It.With.Amqp.NetworkClient
 {
@@ -11,11 +9,9 @@ namespace Test.It.With.Amqp.NetworkClient
 
         public event EventHandler<ReceivedEventArgs> BufferReceived;
         public event EventHandler Disconnected;
-        private readonly ILogger _logger = LogFactory.Create<InternalRoutedNetworkClient>();
 
         public void Send(byte[] buffer, int offset, int count)
         {
-            _logger.Debug($"Sending: {Encoding.UTF8.GetString(buffer)}");
             SendReceived?.Invoke(this, new ReceivedEventArgs(buffer, offset, count));
         }
 
@@ -31,13 +27,11 @@ namespace Test.It.With.Amqp.NetworkClient
                 return;
             }
             _disconnected = true;
-            _logger.Debug("Disconnecting.");
             Disconnected?.Invoke(this, null);
         }
 
         public void TriggerReceive(object sender, ReceivedEventArgs e)
         {
-            _logger.Debug($"Receiving: {Encoding.UTF8.GetString(e.Buffer)}");
             BufferReceived?.Invoke(sender, e);
         }
     }
