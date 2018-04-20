918acae8dd3bd5b082c93f18cb6f0d366b8f4d86
Fresa <fresa@fresa.se>
2018-04-20 19:51:52 +0200

    mvoed rabbitmq 091 specific code to separate repo

--- Tests/Test.It.With.RabbitMQ.Integration.Tests/TestApplication/DefaultRabbitMqPublisherSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Test.It.With.RabbitMQ.Integration.Tests.TestApplication
-{
-    internal class DefaultRabbitMqPublisherSettings : IRabbitMqPublisherSettings
-    {
-        public bool EnablePublisherConfirms { get; set; }
-    }
-}
--- Tests/Test.It.With.RabbitMQ.Integration.Tests/TestApplication/IRabbitMqPublisherSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Test.It.With.RabbitMQ.Integration.Tests.TestApplication
-{
-    internal interface IRabbitMqPublisherSettings
-    {
-        bool EnablePublisherConfirms { get; }
-    }
-}
